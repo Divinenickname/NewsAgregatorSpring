6df07ca6576bd525c2eda0e457ac96480f7eebc5
Alexander <divinenickname@gmail.com>
2020-08-18 13:50:18 +0500

    refactored

--- src/main/java/com/example/parser/ScheduledTasks.java
+++ src/main/java/com/example/parser/ScheduledTasks.java
@@ -1,6 +1,8 @@
 package com.example.parser;
 
+import com.example.exception.URLNotFoundInDBException;
 import com.example.repos.NewsRepo;
+import com.example.repos.SiteRepo;
 import com.rometools.rome.io.FeedException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -11,18 +13,23 @@ import java.io.IOException;
 @Component
 public class ScheduledTasks {
 
+
+
+    private final RSSParser parser;
+    private final VCRUParser vcruParser;
+
     private NewsRepo newsRepo;
-    private RSSParser parser;
+    private SiteRepo siteRepo;
 
     @Autowired
-    public ScheduledTasks(NewsRepo newsRepo){
-        this.newsRepo = newsRepo;
-        parser = new RSSParser(newsRepo);
+    public ScheduledTasks(NewsRepo newsRepo, SiteRepo siteRepo){
+        parser = new RSSParser(newsRepo, siteRepo);
+        vcruParser = new VCRUParser(newsRepo, siteRepo);
     }
 
-    @Scheduled(fixedRate = 5000)
-    public void RSSScheduler() throws IOException, FeedException {
-        parser.parseRss("https://news.yandex.ru/auto.rss");
-        parser.parseRss("https://news.yandex.ru/politics.rss");
+    @Scheduled(fixedRate = 10000)
+    public void RSSScheduler() throws IOException, FeedException, URLNotFoundInDBException {
+        parser.parse("https://news.yandex.ru/auto.rss");
+        vcruParser.parse("https://vc.ru");
     }
 }
