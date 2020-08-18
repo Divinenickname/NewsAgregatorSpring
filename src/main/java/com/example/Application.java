1eca617df92f0ce851e7c9ce8f28ce399744ed61
Alexander <divinenickname@gmail.com>
2020-08-19 00:19:05 +0500

    WTF without @compscan it works unstable

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -1,14 +1,16 @@
 package com.example;
 
-import com.example.parser.ScheduledTasks;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
 
 //@ImportResource("/integration/integration.xml")
 @EnableScheduling
 @SpringBootApplication
+@ComponentScan({"com.example.domain", "com.example.parser", "com.example.services", "com.example.repos", "com.example.parser"})
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
