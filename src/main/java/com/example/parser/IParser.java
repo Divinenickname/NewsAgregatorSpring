217039ccef3b54265cc9db4c4a9f186cc4c26f70
Alexander <divinenickname@gmail.com>
2020-08-20 00:32:57 +0500

    Interface javadoc added

--- src/main/java/com/example/parser/IParser.java
+++ src/main/java/com/example/parser/IParser.java
@@ -6,6 +6,9 @@ import com.rometools.rome.io.FeedException;
 import java.io.IOException;
 import java.util.List;
 
+/**
+ * @return list of parsed news
+ */
 public interface IParser {
     List<News> parse() throws IOException, FeedException;
 
