f0b6bb472552ff23fc0cdac330267b69be92ec64
Alexander <divinenickname@gmail.com>
2020-08-20 12:39:56 +0500

    fix returned type for findAll useless imports have been removed

--- src/main/java/com/example/services/NewsService.java
+++ src/main/java/com/example/services/NewsService.java
@@ -1,14 +1,11 @@
 package com.example.services;
 
 import com.example.domain.News;
-import com.example.domain.dto.NewsDTO;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface NewsService {
     List<News> removeDublicates(List<News> newsList);
     void saveNewsList(List<News> newsList);
-    List<NewsDTO> findAll();
+    List<News> findAll();
 }
