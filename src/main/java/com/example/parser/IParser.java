20d73fa2a2a8f164e7e99c4f56c171301d7f9564
Alexander <divinenickname@gmail.com>
2020-08-20 13:05:06 +0500

    fix javadoc

--- src/main/java/com/example/parser/IParser.java
+++ src/main/java/com/example/parser/IParser.java
@@ -1,15 +1,16 @@
 package com.example.parser;
 
 import com.example.domain.News;
-import com.rometools.rome.io.FeedException;
 
 import java.io.IOException;
 import java.util.List;
 
-/**
- * @return list of parsed news
- */
 public interface IParser {
-    List<News> parse() throws IOException, FeedException;
+    /**
+     * Just parse and return list of news
+     * @return List of parsed news
+     * @throws IOException can be thrown by url connection
+     */
+    List<News> parse() throws IOException;
 
 }
